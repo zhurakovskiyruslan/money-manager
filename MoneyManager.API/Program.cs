9603b79144e9a4e070b045fd7932202cdcf8748a
zhurakovskiyruslan <zhurakovskiyruslan@gmail.com>
2025-09-23 16:49:13 +0300

    adding controller for account and set program.cs

---
+++ MoneyManager.API/Program.cs
@@ -0,0 +1,42 @@
+    using FluentValidation;
+    using MediatR;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using MoneyManager.Application.Accounts.Commands.CreateAccount;
+    using MoneyManager.Application.Common.Behaviors;
+    using MoneyManager.Infrastructure;
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.Services.AddMediatR(cfg =>
+    cfg.RegisterServicesFromAssembly(typeof(CreateAccountCommand).Assembly));
+    builder.Services.AddValidatorsFromAssembly(typeof(CreateAccountCommandValidator).Assembly);
+    
+    builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+    
+    var connectionString = "Host=localhost;Port=5432;Database=MoneyManager;Username=postgres;Password=superuser";
+                           
+
+    builder.Services.AddInfrastructure(connectionString);
+
+    var app = builder.Build();
+
+   
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "Money Manager V1");
+            c.RoutePrefix = string.Empty;
+        });
+    
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
