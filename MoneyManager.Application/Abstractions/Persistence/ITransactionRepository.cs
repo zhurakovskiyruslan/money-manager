46f55b085f00c7a155a7fe1714d3ef02a46417a5
zhurakovskiyruslan <zhurakovskiyruslan@gmail.com>
2025-10-02 09:55:05 +0300

    adding interface and implementation for transaction queries

---
+++ MoneyManager.Application/Abstractions/Persistence/ITransactionRepository.cs
@@ -0,0 +1,9 @@
+using MoneyManager.Domain.Entities;
+
+namespace MoneyManager.Application.Abstractions.Persistence;
+
+public interface ITransactionRepository
+{
+    Task<IReadOnlyList<Transaction>> GetUserTransactionsAsync(Guid userId, CancellationToken ct);
+    Task<IReadOnlyList<Transaction>> GetAccountTransactionsAsync(Guid accountId, CancellationToken ct);
+}
